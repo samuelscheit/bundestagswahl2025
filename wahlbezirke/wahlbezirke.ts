5b8e4c51c7e43ddd28c8a3edf26a5723c1f16e09
Paul Menzel <pmenzel@molgen.mpg.de>
2025-03-12 13:05:05 +0100

    Revert "fix: #10"
    This reverts commit 82f456663847be5c94b15ba9497f219437d00b31.

--- wahlbezirke/wahlbezirke.ts
+++ wahlbezirke/wahlbezirke.ts
@@ -139,10 +139,6 @@ export async function axiosWithRedirect<T = any, D = any>(
 		return axiosWithRedirect(newUrl, opts);
 	}
 
-	if (typeof response.data === "object" && response.data && response.data instanceof Buffer) {
-		response.data = response.data.toString("utf-8");
-	}
-
 	return { ...response, url };
 }
 
